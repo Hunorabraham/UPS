f071d8302fb13ae6cd333ea6573aae523ec8792e
Hunorabraham <hunor335@gmail.com>
2024-05-31 11:28:41 +0200

    sprite loading

--- script.js
+++ script.js
@@ -7,11 +7,27 @@ bgGradient.addColorStop(0.9,"rgb(85,112,95)");
 bgGradient.addColorStop(1,"black");
 const pixelWidth = 10;
 const sourceMultiplier = pixelWidth/5;
-const testSprite = new Image();
-testSprite.src = "./img/test.png";
-testSprite.onload = start;
 
 
+let spriteDict = {};
+let spriteCount = 0;
+function addSprite(name, filename){
+    let img = new Image();
+    img.src = `./img/${filename}`;
+    img.onload = load;
+    spriteDict[name] = img;
+    spriteCount++;
+}
+let loadedSprites = 0;
+let running = false;
+function load(){
+    loadedSprites++;
+    console.log("thing loaded: ", this);
+    if(loadedSprites>=spriteCount && !running){
+        running = false;
+        start();
+    }
+}
 function drawGrid(){
     draw.beginPath();
     for(let i = 1; i < can.height / pixelWidth; i++){
@@ -29,16 +45,31 @@ function drawGrid(){
 function drawPixelPerfect(img, x, y){
     draw.drawImage(img, x * pixelWidth, y * pixelWidth, img.width * sourceMultiplier, img.height * sourceMultiplier);
 }
+
+//initilalisation
+addSprite("planet_1","planet.png");
+addSprite("planet_2","planet2.png");
+
+
 function start(){
+    //this whole part is only for testing
+    console.log("begun!", spriteDict);
     let x = 5;
     draw.lineWidth = 2;
     draw.fillStyle = bgGradient;
     draw.strokeStyle = bgGradient;
+    planets = [];
+    for(let i = 0; i < 5; i++){
+        planets.push({
+            x: Math.round(Math.random()*can.width/pixelWidth),
+            y: Math.round(Math.random()*can.height/pixelWidth),
+            type: (Math.random()<0.5)?"planet_1":"planet_2",
+        });
+    }
     setInterval(()=>{
         draw.fillRect(0,0,can.width,can.height);
-        drawPixelPerfect(testSprite, x++, 5);
+        //drawPixelPerfect(spriteDict.planet_1,5,5);
+        planets.forEach(planet => drawPixelPerfect(spriteDict[planet.type],planet.x,planet.y));
         drawGrid();
     },100);
 }
-
-
