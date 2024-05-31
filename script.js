24b73e45bdede08f88e40623d8b5347ec3de2a75
Hunorabraham <hunor335@gmail.com>
2024-05-31 11:08:06 +0200

    added images, created drawing related variables

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const can = document.querySelectorAll("canvas")[0];
 const draw = can.getContext("2d");
 const bgColour = "rgb(170,225,190)";
+const bgGradient = draw.createRadialGradient(can.width/2,can.height/2,can.height/10,can.width/2,can.height/2,can.height);
+bgGradient.addColorStop(0,"rgb(170,225,190)");
+bgGradient.addColorStop(0.9,"rgb(85,112,95)");
+bgGradient.addColorStop(1,"black");
 const pixelWidth = 10;
 const sourceMultiplier = pixelWidth/5;
 const testSprite = new Image();
@@ -9,7 +13,6 @@ testSprite.onload = start;
 
 
 function drawGrid(){
-    draw.strokeStyle = bgColour;
     draw.beginPath();
     for(let i = 1; i < can.height / pixelWidth; i++){
         draw.moveTo(0, i * pixelWidth);
@@ -28,8 +31,10 @@ function drawPixelPerfect(img, x, y){
 }
 function start(){
     let x = 5;
+    draw.lineWidth = 2;
+    draw.fillStyle = bgGradient;
+    draw.strokeStyle = bgGradient;
     setInterval(()=>{
-        draw.fillStyle = bgColour;
         draw.fillRect(0,0,can.width,can.height);
         drawPixelPerfect(testSprite, x++, 5);
         drawGrid();
